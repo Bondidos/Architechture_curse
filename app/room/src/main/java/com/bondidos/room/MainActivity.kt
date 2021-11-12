6ff88615bd3536a9daf867c624c2ececdaebc08e
Bondidos <gglbond@gmail.com>
2021-11-12 21:09:38 +0300

    lesson 9

--- app/room/src/main/java/com/bondidos/room/MainActivity.kt
+++ app/room/src/main/java/com/bondidos/room/MainActivity.kt
@@ -1,10 +1,26 @@
 package com.bondidos.room
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.MainThread
+import androidx.annotation.RequiresApi
 import com.bondidos.room.dao.EmployeeDao
 import com.bondidos.room.entityes.Employee
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.FlowableOnSubscribe
+import io.reactivex.FlowableSubscriber
+import io.reactivex.Observer
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
+import io.reactivex.observers.DisposableMaybeObserver
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import org.reactivestreams.Subscriber
+import java.util.concurrent.Flow
 import javax.inject.Inject
 
 
@@ -15,10 +31,57 @@ class MainActivity : AppCompatActivity() {
     lateinit var db: AppDataBase
     lateinit var employeeDao: EmployeeDao
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         employeeDao = db.employeeDao()
+
+        employeeDao.getAll()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                //....
+            }
+
+        employeeDao.getById(1)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe{
+                //  ....
+            }
+
+        employeeDao.getByIdSingle(2)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : DisposableSingleObserver<Employee>(){
+
+                override fun onSuccess(t: Employee) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onError(e: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+
+        employeeDao.getByIdMaybe(3)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : DisposableMaybeObserver<Employee>(){
+
+                override fun onSuccess(t: Employee) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onError(e: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onComplete() {
+                    TODO("Not yet implemented")
+                }
+
+            })
     }
 }
--- app/room/src/main/java/com/bondidos/room/dao/EmployeeDao.kt
+++ app/room/src/main/java/com/bondidos/room/dao/EmployeeDao.kt
@@ -3,15 +3,24 @@ package com.bondidos.room.dao
 import androidx.room.*
 import androidx.room.OnConflictStrategy.REPLACE
 import com.bondidos.room.entityes.Employee
+import io.reactivex.Flowable
+import io.reactivex.Maybe
+import io.reactivex.Single
 
 @Dao
 interface EmployeeDao {
 
     @Query("SELECT * FROM employees")
-    fun getAll(): List<Employee>
+    fun getAll(): Flowable<List<Employee>>
 
     @Query("SELECT * FROM employees where id = :id")
-    fun getById(id: Long): Employee
+    fun getById(id: Long): Flowable<Employee>
+
+    @Query("SELECT * FROM employees where id = :id")
+    fun getByIdSingle(id: Long): Single<Employee>
+
+    @Query("SELECT * FROM employees where id = :id")
+    fun getByIdMaybe(id: Long): Maybe<Employee>
 
     @Insert(onConflict = REPLACE)
     fun insert(employee: Employee)
